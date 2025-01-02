cf2a1e635a6c62ceb9c679fed71485a43761ea3d
rahul4732saini <rahul4732saini@gmail.com>
2025-01-02 18:09:33 +0530

    Define board deletion function in board.c
    Defined `free_board` function for freeing up the
    dynamically allocated memory for the game board.

--- src/board.c
+++ src/board.c
@@ -12,3 +12,11 @@ cell_t **create_board(size_t size)
 
     return board;
 }
+
+void free_board(cell_t **board, size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+        free(board[i]);
+
+    free(board);
+}
