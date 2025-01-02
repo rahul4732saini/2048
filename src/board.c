6182b697ee59aa348e2b8870d063cb1db125849d
rahul4732saini <rahul4732saini@gmail.com>
2025-01-02 18:05:13 +0530

    Define game board creation function in board.c
    Defined `create_board` function for dynamically
    creating the 2048 game board during runtime.

---
+++ src/board.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "shared.h"
+
+cell_t **create_board(size_t size)
+{
+    cell_t **board = (cell_t **)malloc(size * sizeof(cell_t *));
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        board[i] = (cell_t *)calloc(size, sizeof(cell_t));
+    }
+
+    return board;
+}
