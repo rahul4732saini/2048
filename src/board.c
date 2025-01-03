5944d0cb2e5312b0fdad7c2f15533b2734010db8
rahul4732saini <rahul4732saini@gmail.com>
2025-01-03 20:02:32 +0530

    Define new function in board.c
    Defined `place_random` function for randomly
    placing the value 2 in a cell on the game board.

--- src/board.c
+++ src/board.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include "shared.h"
 
 cell_t **create_board(size_t size)
@@ -11,6 +13,31 @@ cell_t **create_board(size_t size)
     }
 
     return board;
+}
+
+bool place_random(struct Game *game)
+{
+    size_t positions[game->bsize * game->bsize];
+    size_t ctr = 0;
+
+    for (size_t i = 0; i < game->bsize; ++i)
+    {
+        for (size_t j = 0; j < game->bsize; ++j)
+        {
+            if (game->board[i][j])
+                continue;
+
+            positions[ctr++] = i * game->bsize + j;
+        }
+    }
+
+    if (!ctr)
+        return false;
+
+    size_t position = positions[rand() % ctr];
+    game->board[position / game->bsize][position % game->bsize] = 2;
+
+    return true;
 }
 
 void free_board(struct Game *game)
