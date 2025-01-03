410f70c0f19cccc694fb3a5f52e8269a462fd1f2
rahul4732saini <rahul4732saini@gmail.com>
2025-01-03 20:03:51 +0530

    Add signature for place_random function in board.h

--- src/include/board.h
+++ src/include/board.h
@@ -1,9 +1,11 @@
 #ifndef _BOARD_H
 #define _BOARD_H
 
+#include <stdbool.h>
 #include "shared.h"
 
 cell_t **create_board(size_t size);
+bool place_random(struct Game *game);
 void free_board(struct Game *game);
 
 #endif
