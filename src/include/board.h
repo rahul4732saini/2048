37895add53c869f68da6846bfbda9d15db4da78f
rahul4732saini <rahul4732saini@gmail.com>
2025-01-03 20:56:17 +0530

    Add signature for move_vertical function in board.h

--- src/include/board.h
+++ src/include/board.h
@@ -7,6 +7,7 @@
 cell_t **create_board(size_t size);
 void add_horizontal(struct Game *game, bool left);
 void move_horizontal(struct Game *game, bool left);
+void move_vertical(struct Game *game, bool left);
 bool place_random(struct Game *game);
 void free_board(struct Game *game);
 
