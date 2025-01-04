ca02e750c1cd6c6adbefcaf2cdbdf2d087e4dc84
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 20:39:54 +0530

    Define constant variables in consts.c

---
+++ src/consts.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+const char *header = "2048 Game";
+const char *footers[] = {
+    "Made by Rahul Saini",
+    "Visit https://www.github.com/rahul4732saini/2048 for more info",
+};
+
+const size_t footers_size = 2;
