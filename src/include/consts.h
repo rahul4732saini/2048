35821228dd4c28bfc7837d226c151efc6389cc57
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 19:58:42 +0530

    Defined constants in consts.h
    Defined constant variables for the screen header and footers.

---
+++ src/include/consts.h
@@ -0,0 +1,14 @@
+#ifndef _CONSTS_H
+#define _CONSTS_H
+
+#include <stdlib.h>
+
+const char *header = "2048 Game";
+const char *footers[] = {
+    "Made by Rahul Saini",
+    "Visit https://www.github.com/rahul4732saini/2048 for more info",
+};
+
+const size_t footers_size = 2;
+
+#endif
