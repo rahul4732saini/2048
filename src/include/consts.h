4c098d29136c3906ac49de411bcd32d51058ae5c
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 20:56:49 +0530

    Refactor consts.h based on recent changes made in consts.c

--- src/include/consts.h
+++ src/include/consts.h
@@ -3,9 +3,13 @@
 
 #include <stdlib.h>
 
-extern const char *header;
+extern const char *headers[];
 extern const char *footers[];
 
+extern const size_t headers_size;
 extern const size_t footers_size;
+
+extern const size_t win_vertical_margins;
+extern const size_t win_horizontal_margins;
 
 #endif
