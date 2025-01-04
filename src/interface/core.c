76a64525d54c55f9b4cd93192ae9d04a54d67a6e
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 20:11:04 +0530

    Define new function in core.c
    Defined `display_header_footer` function for
    displaying the header and footer on the TUI screen.

--- src/interface/core.c
+++ src/interface/core.c
@@ -11,3 +11,15 @@ void initialize_screen(void)
     cbreak();
     noecho();
 }
+
+void display_header_footer(struct Screen *screen)
+{
+    size_t st_pos = (screen->width - strlen(header)) / 2;
+    mvprintw(1, st_pos, "%s", header);
+
+    for (size_t i = 0; i < footers_size; ++i)
+    {
+        st_pos = (screen->width - strlen(footers[i])) / 2;
+        mvprintw(screen->height - footers_size + i - 1, st_pos, "%s", footers[i]);
+    }
+}
