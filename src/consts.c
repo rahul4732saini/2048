9a015b5ec9ed94a8b8230cdf05f585bb8c878c94
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 20:54:55 +0530

    Refactor consts.c
    Redefined the `header` constant as `headers` and initializes it as
    an array comprising the header text lines. Also initializes the
    `headers_size` constant comprising the size of the `headers` array.

--- src/consts.c
+++ src/consts.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 
-const char *header = "2048 Game";
+const char *headers[] = {
+    "2048 Game",
+};
+
 const char *footers[] = {
     "Made by Rahul Saini",
     "Visit https://www.github.com/rahul4732saini/2048 for more info",
 };
 
+const size_t headers_size = 1;
 const size_t footers_size = 2;
