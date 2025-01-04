ce0217deac2a7e164ce73d00c7ed9d175154564f
rahul4732saini <rahul4732saini@gmail.com>
2025-01-04 20:40:44 +0530

    Refactor consts.h

--- src/include/consts.h
+++ src/include/consts.h
@@ -3,12 +3,9 @@
 
 #include <stdlib.h>
 
-const char *header = "2048 Game";
-const char *footers[] = {
-    "Made by Rahul Saini",
-    "Visit https://www.github.com/rahul4732saini/2048 for more info",
-};
+extern const char *header;
+extern const char *footers[];
 
-const size_t footers_size = 2;
+extern const size_t footers_size;
 
 #endif
