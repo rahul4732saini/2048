1c6c08b7fdbe93ea339c29096676a07bdc789a84
rahul4732saini <rahul4732saini@gmail.com>
2025-01-10 17:28:55 +0530

    Added signature for show_game_win function in core.h

--- src/include/interface/core.h
+++ src/include/interface/core.h
@@ -8,6 +8,7 @@ void init_screen(void);
 void init_game_win(WINDOW **window, Screen *screen);
 
 void show_header_footer(Screen *screen);
+void show_game_win(WINDOW *window);
 void show_window_title(WINDOW *window, Screen *screen, const char *title);
 
 #endif
