81b82220785b9521432e9e5d807b02fea33f45ba
rahul4732saini <rahul4732saini@gmail.com>
2025-01-10 17:21:45 +0530

    Reformat board.h

--- src/include/board.h
+++ src/include/board.h
@@ -5,11 +5,14 @@
 #include "shared.h"
 
 cell_t **create_board(size_t size);
+void free_board(Game *game);
+
+bool place_random(Game *game);
+
 void add_horizontal(Game *game, bool left);
 void add_vertical(Game *game, bool left);
+
 void move_horizontal(Game *game, bool left);
 void move_vertical(Game *game, bool left);
-bool place_random(Game *game);
-void free_board(Game *game);
 
 #endif
