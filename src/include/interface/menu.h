b3a0da795e03e6d11e69a8555a8f290c9c1f3ec5
rahul4732saini <rahul4732saini@gmail.com>
2025-01-10 17:58:31 +0530

    Add function signatures in menu.h

--- src/include/interface/menu.h
+++ src/include/interface/menu.h
@@ -5,6 +5,9 @@
 #include "shared.h"
 
 void init_main_menu(WINDOW **window, Screen *screen);
+void init_pause_menu(WINDOW **window, Screen *screen);
+
 void show_main_menu(WINDOW *window, size_t select, int select_color);
+void show_pause_menu(WINDOW *window, size_t select, int select_color);
 
 #endif
