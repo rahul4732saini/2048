89fcaa836000cf20b9893b354f22396a02611e7f
rahul4732saini <rahul4732saini@gmail.com>
2025-01-10 12:36:37 +0530

    Update type references in core.h
    Update references to the struct type `struct Screen` -> `Screen`.

--- src/include/interface/core.h
+++ src/include/interface/core.h
@@ -1,10 +1,11 @@
 #ifndef _CORE_H
 #define _CORE_H
 
+#include <ncurses.h>
 #include "shared.h"
 
 void init_screen(void);
-void init_game_win(WINDOW **window, struct Screen *screen);
-void display_header_footer(struct Screen *screen);
+void init_game_win(WINDOW **window, Screen *screen);
+void display_header_footer(Screen *screen);
 
 #endif
