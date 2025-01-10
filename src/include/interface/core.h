de92adc210f5d10ac427561c5c8ebb43bb4d6a00
rahul4732saini <rahul4732saini@gmail.com>
2025-01-10 17:13:55 +0530

    Add signature for show_window_title function in core.h

--- src/include/interface/core.h
+++ src/include/interface/core.h
@@ -7,5 +7,6 @@
 void init_screen(void);
 void init_game_win(WINDOW **window, Screen *screen);
 void display_header_footer(Screen *screen);
+void show_window_title(WINDOW *window, Screen *screen, const char *title);
 
 #endif
