30575083b2c119c1349ad5479519e171d9d6fcf8
rahul4732saini <rahul4732saini@gmail.com>
2025-01-11 15:59:53 +0530

    Define draw_vlines function in board.c
    Defined the function for drawing the vertical lines seperating
    game cells horizontally for each individual row in the game board.

--- src/interface/board.c
+++ src/interface/board.c
@@ -21,3 +21,20 @@ void init_board(WINDOW **window, Screen *screen, size_t bsize)
 
     keypad(*window, TRUE);
 }
+
+static void draw_vlines(WINDOW *window, size_t bsize)
+{
+    size_t y, _;
+    getyx(window, y, _);
+
+    for (size_t i = 0; i < cell_height; ++i)
+    {
+        wmove(window, y + i, 1);
+
+        for (size_t j = 0; j < bsize - 1; ++j)
+        {
+            wprintw(window, "%*s", cell_width, "");
+            waddch(window, ACS_VLINE);
+        }
+    }
+}
