2833e8b216c66d9fa3597d7da5233e2d4cd919a8
rahul4732saini <rahul4732saini@gmail.com>
2025-01-23 21:30:20 +0530

    Rename the window parameter in functions within board.c
    Renamed the `window` parameter for storing the board window to
    a more short and concise name `win` in all functions using it.

--- src/interface/board.c
+++ src/interface/board.c
@@ -19,69 +19,69 @@ void init_board(Screen *scr, Screen *parent, size_t bsize)
     scr->window = init_window(dim_scr);
 }
 
-static void draw_vlines(WINDOW *window, size_t bsize)
+static void draw_vlines(WINDOW *win, size_t bsize)
 {
-    size_t y = getcury(window);
+    size_t y = getcury(win);
 
     for (size_t i = 0; i < cell_height; ++i)
     {
-        wmove(window, y + i, 1);
+        wmove(win, y + i, 1);
 
         for (size_t j = 0; j < bsize - 1; ++j)
         {
-            wprintw(window, "%*s", cell_width, "");
-            waddch(window, ACS_VLINE);
+            wprintw(win, "%*s", cell_width, "");
+            waddch(win, ACS_VLINE);
         }
     }
 }
 
-static void draw_hlines(WINDOW *window, size_t bsize)
+static void draw_hlines(WINDOW *win, size_t bsize)
 {
     for (size_t i = 0; i < bsize; ++i)
     {
         for (size_t j = 0; j < cell_width; ++j)
-            waddch(window, ACS_HLINE);
+            waddch(win, ACS_HLINE);
 
         if (i != bsize - 1)
-            waddch(window, ACS_PLUS);
+            waddch(win, ACS_PLUS);
     }
 }
 
-static void draw_edges(WINDOW *window, size_t bsize)
+static void draw_edges(WINDOW *win, size_t bsize)
 {
     size_t height = bsize * (cell_height + 1) + 1;
     size_t width = bsize * (cell_width + 1) + 1;
 
     for (size_t i = 1; i < bsize; ++i)
     {
-        mvwaddch(window, 0, (cell_width + 1) * i, ACS_TTEE);
-        mvwaddch(window, height - 1, (cell_width + 1) * i, ACS_BTEE);
+        mvwaddch(win, 0, (cell_width + 1) * i, ACS_TTEE);
+        mvwaddch(win, height - 1, (cell_width + 1) * i, ACS_BTEE);
 
-        mvwaddch(window, (cell_height + 1) * i, 0, ACS_LTEE);
-        mvwaddch(window, (cell_height + 1) * i, width - 1, ACS_RTEE);
+        mvwaddch(win, (cell_height + 1) * i, 0, ACS_LTEE);
+        mvwaddch(win, (cell_height + 1) * i, width - 1, ACS_RTEE);
     }
 }
 
-static void draw_grid(WINDOW *window, size_t bsize)
+static void draw_grid(WINDOW *win, size_t bsize)
 {
-    draw_edges(window, bsize);
+    draw_edges(win, bsize);
 
     for (size_t i = 0; i < bsize; ++i)
     {
-        wmove(window, (cell_height + 1) * i + 1, 1);
-        draw_vlines(window, bsize);
+        wmove(win, (cell_height + 1) * i + 1, 1);
+        draw_vlines(win, bsize);
 
         if (i == bsize - 1)
             continue;
 
-        wmove(window, (i + 1) * (cell_height + 1), 1);
-        draw_hlines(window, bsize);
+        wmove(win, (i + 1) * (cell_height + 1), 1);
+        draw_hlines(win, bsize);
     }
 
-    wrefresh(window);
+    wrefresh(win);
 }
 
-static void populate_cells(WINDOW *window, Game *game)
+static void populate_cells(WINDOW *win, Game *game)
 {
     size_t pos_x, pos_y, num_len;
 
@@ -97,12 +97,12 @@ static void populate_cells(WINDOW *window, Game *game)
             pos_x = j * (cell_width + 1) + (cell_width - num_len) / 2 + 1;
             pos_y = i * (cell_height + 1) + cell_height / 2 + 1;
 
-            wmove(window, pos_y, pos_x);
-            wprintw(window, "%d", game->board[i][j]);
+            wmove(win, pos_y, pos_x);
+            wprintw(win, "%d", game->board[i][j]);
         }
     }
 
-    wrefresh(window);
+    wrefresh(win);
 }
 
 void show_board(Screen *scr, Game *game)
