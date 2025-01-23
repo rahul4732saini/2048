654ff0269022659bb288c46cf5e6a4f7fbd7bbc5
rahul4732saini <rahul4732saini@gmail.com>
2025-01-23 21:54:56 +0530

    Refactor function signatures in menu.h
    Refactored the function signatures based on the recent
    changes made in the associated C source file.

--- src/include/interface/menu.h
+++ src/include/interface/menu.h
@@ -4,10 +4,10 @@
 #include <ncurses.h>
 #include "shared.h"
 
-void init_main_menu(WINDOW **window, Screen *screen);
-void init_pause_menu(WINDOW **window, Screen *screen);
+void init_main_menu(Screen *scr, Screen *parent);
+void init_pause_menu(Screen *scr, Screen *parent);
 
-void show_main_menu(WINDOW *window, size_t select, int select_color);
-void show_pause_menu(WINDOW *window, size_t select, int select_color);
+void show_main_menu(Screen *scr, size_t select, int select_color);
+void show_pause_menu(Screen *scr, size_t select, int select_color);
 
 #endif
