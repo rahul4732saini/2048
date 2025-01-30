878af8088d3de3e50f1d1d6fd9d4d6ffc2f798df
rahul4732saini <rahul4732saini@gmail.com>
2025-01-30 19:32:38 +0530

    Define handle_pause_menu function in core.c
    Defined the function for handling the placement of the associated
    window on the screen and managing user input on the pause menu.

--- src/core.c
+++ src/core.c
@@ -36,3 +36,34 @@ int8_t handle_main_menu(Screen *scrs)
 
     return select;
 }
+
+int8_t handle_pause_menu(Screen *scrs)
+{
+    int16_t input = 0;
+    int8_t select = 0;
+
+    place_pause_menu(scrs + 2, scrs + 1);
+    show_pause_menu(scrs + 2, select, 1);
+
+    while ((input = getch()) != 10)
+    {
+        switch (input)
+        {
+        case KEY_UP:
+            --select;
+            break;
+
+        case KEY_DOWN:
+            ++select;
+            break;
+
+        case KEY_RESIZE:
+            return -1;
+        }
+
+        select = (select + pause_menu_items_size) % pause_menu_items_size;
+        show_pause_menu(scrs + 2, select, 1);
+    }
+
+    return select;
+}
