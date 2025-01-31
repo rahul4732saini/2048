3f385f27fec691cf3ba87cbf6d0838f3f5f1ab27
rahul4732saini <rahul4732saini@gmail.com>
2025-01-31 20:01:23 +0530

    Define handle_game_board function in core.c
    Defined the function for handling the game
    board window and managing user input.

--- src/core.c
+++ src/core.c
@@ -1,9 +1,12 @@
 #include <ncurses.h>
 #include <stdint.h>
+#include <stdbool.h>
 
+#include "logic.h"
 #include "shared.h"
 #include "consts.h"
 
+#include "interface/board.h"
 #include "interface/menu.h"
 
 int8_t handle_main_menu(Screen *scrs)
@@ -66,4 +69,51 @@ int8_t handle_pause_menu(Screen *scrs)
     }
 
     return select;
+}
+
+int8_t handle_game_board(Screen *scrs, Game *game)
+{
+    int16_t input, operations;
+    bool iskey, isempty;
+
+    place_board(scrs + 2, scrs, game->bsize);
+    show_board(scrs + 2, game);
+
+    while ((input = getch()) != KEY_EXIT)
+    {
+        operations = 0;
+        isempty = true;
+
+        switch (input)
+        {
+        case KEY_UP:
+        case KEY_DOWN:
+            iskey = input == KEY_UP;
+
+            operations += add_vertical(game, iskey);
+            operations += move_vertical(game, iskey);
+            break;
+
+        case KEY_LEFT:
+        case KEY_RIGHT:
+            iskey = input == KEY_LEFT;
+
+            operations += add_horizontal(game, iskey);
+            operations += move_horizontal(game, iskey);
+            break;
+
+        case KEY_RESIZE:
+            return -1;
+        }
+
+        if (operations)
+            isempty = place_random(game);
+
+        if (game_over(game, isempty) || game->max_val == 2048)
+            return 0;
+
+        show_board(scrs + 2, game);
+    }
+
+    return 1;
 }
