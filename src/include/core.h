78bfb048613b4030f989ab11de59b6e2d5607214
rahul4732saini <rahul4732saini@gmail.com>
2025-01-31 20:05:28 +0530

    Add function signatures in core.h

--- src/include/core.h
+++ src/include/core.h
@@ -1,4 +1,10 @@
-#define _CORE_H
 #ifndef _CORE_H
+#define _CORE_H
+
+#include "shared.h"
+
+int8_t handle_main_menu(Screen *scrs);
+int8_t handle_pause_menu(Screen *scrs);
+int8_t handle_game_board(Screen *scrs, Game *game);
 
 #endif
