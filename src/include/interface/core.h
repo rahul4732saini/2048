ba2431107fdb981ea32e52a9e7b557f2af07de49
rahul4732saini <rahul4732saini@gmail.com>
2025-02-06 16:18:32 +0530

    Add signature for show_dialog function in core.h

--- src/include/interface/core.h
+++ src/include/interface/core.h
@@ -11,6 +11,7 @@ void place_game_win(Screen *scr, Screen *parent);
 void show_header_footer(Screen *scr);
 void show_game_win(Screen *scr);
 void show_window_title(Screen *scr, const char *title);
+void show_dialog(Screen *scr, const char *mesg[], size_t mesg_len, int select_color);
 
 void refresh_game_win(Screen *game_scr, Screen *core_scr, const char *title);
 
