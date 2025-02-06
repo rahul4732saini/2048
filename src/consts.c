9b0a0cd012ec4b12a78d0aae2fb708d62b168aea
rahul4732saini <rahul4732saini@gmail.com>
2025-02-06 19:56:09 +0530

    Reformat and group constant definitions in consts.c

--- src/consts.c
+++ src/consts.c
@@ -21,26 +21,24 @@ const size_t win_vertical_margins = headers_size + footers_size + 4;
 const size_t win_horizontal_margins = 0;
 
 const char *main_menu_title = "Main Menu";
-const char *main_menu_items[] = {"Play", "Quit"};
-
-const size_t main_menu_items_size = 2;
-const size_t main_menu_width = 20;
-
 const char *pause_menu_title = "Pause Menu";
+const char *main_menu_items[] = {"Play", "Quit"};
 const char *pause_menu_items[] = {
     "Resume",
     "Quit to Main Menu",
     "Quit Game",
 };
 
+const size_t main_menu_items_size = 2;
 const size_t pause_menu_items_size = 3;
+const size_t main_menu_width = 20;
 const size_t pause_menu_width = 25;
 
 const size_t dialog_bt_width = 10;
 const char *dialog_bt_txt = "OK";
 
 const char *win_dialog_txt[] = {"You won the game!"};
-const size_t win_dialog_txt_len = 1;
-
 const char *lost_dialog_txt[] = {"You lost the game!", "Better luck next time..."};
+
+const size_t win_dialog_txt_len = 1;
 const size_t lost_dialog_txt_len = 2;
