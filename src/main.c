5f103e382cbaa8c3e9afa369248935e5edb36333
rahul4732saini <rahul4732saini@gmail.com>
2025-02-07 19:55:20 +0530

    Refactor main.c
    Replace instances of hard-coded integers for initializing and cleaning
    the Screen sturcts with reference to the `screen_count` constants allowing
    for flexibility in modifition.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,24 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
-#include "core.h"
 #include "shared.h"
+#include "core.h"
+#include "consts.h"
 
 #include "interface/common.h"
 #include "interface/core.h"
 
 Screen *setup(void)
 {
-    Screen *scrs = (Screen *)calloc(3, sizeof(Screen));
+    Screen *scrs = (Screen *)calloc(screen_count, sizeof(Screen));
 
-    for (size_t i = 0; i < 3; ++i)
+    for (size_t i = 0; i < screen_count; ++i)
         scrs[i].dimension = (Dimension *)malloc(sizeof(Dimension));
 
     init_screen(scrs);
     get_screen_dimension(scrs);
 
-    for (size_t i = 1; i < 3; ++i)
+    for (size_t i = 1; i < screen_count; ++i)
         scrs[i].window = init_window();
 
     return scrs;
@@ -25,7 +26,7 @@ Screen *setup(void)
 
 void clean(Screen *scrs)
 {
-    for (size_t i = 0; i < 3; ++i)
+    for (size_t i = 0; i < screen_count; ++i)
     {
         delwin(scrs[i].window);
         free(scrs[i].dimension);
