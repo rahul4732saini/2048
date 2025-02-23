2574d63061adb1ab252a968985cd6676971a13a0
rahul4732saini <rahul4732saini@gmail.com>
2025-02-23 16:55:29 +0530

    Add file and function documentation in shared.c

--- src/interface/shared.c
+++ src/interface/shared.c
@@ -1,6 +1,19 @@
+/**
+ * @file shared.c
+ * @brief Defines shared functions for common TUI interface routines.
+ *
+ * This module defines functions used for performing actions common
+ * to and reuiqred by all the TUI interface handler functions defined
+ * within other modules.
+ */
+
 #include <ncurses.h>
 #include "shared.h"
 
+/**
+ * @brief Initializes a new ncurses TUI window.
+ * @return Pointer to the newly created window.
+ */
 WINDOW *init_window(void)
 {
     WINDOW *win = newwin(0, 0, 0, 0);
@@ -11,6 +24,12 @@ WINDOW *init_window(void)
     return win;
 }
 
+/**
+ * @brief Positions and resizes the specified TUI window.
+ *
+ * This function resizes the window dimensions, and moves it
+ * to the coordinates stored within the Dimension struct.
+ */
 void place_window(Screen *scr)
 {
     Dimension *dim = scr->dimension;
