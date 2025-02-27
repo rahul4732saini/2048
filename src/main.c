177e557ed20350e51d38664cc9e83d3f343d0748
rahul4732saini <rahul4732saini@gmail.com>
2025-02-27 19:52:21 +0530

    Refactor documentation and comments in main.c

--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
  * @file main.c
  * @brief Entry point for the TUI-based 2048 game.
  *
- * This file contains the main function and the core setup and cleanup
- * routines for the text-based user interface (TUI) using ncurses. It
- * initializes the TUI environment, manages the game loop, and ensures
- * proper resource cleanup before exiting.
+ * @details This file comprises the main function and the core setup and cleanup
+ *          routines for the text-based user interface (TUI) using ncurses. It
+ *          initializes the TUI environment, manages the game loop, and ensures
+ *          proper resource cleanup before exiting.
  *
  * @author rahul4732saini
  */
@@ -24,9 +24,9 @@
 /**
  * @brief Initializes the TUI environment for the game.
  *
- * Sets up the Screen and Dimensions structures for encapsulating the TUI
- * windows and their associated dimensions. It also initializes the standard
- * TUI screen and other game windows.
+ * @details Sets up the Screen and Dimensions structures for encapsulating the TUI
+ *          windows and their associated dimensions. It also initializes the standard
+ *          TUI screen and other game windows.
  *
  * @return A pointer to the first Screen in the dynamically allocated screens
  *         array, representing the TUI windows.
@@ -41,6 +41,7 @@ Screen *setup(void)
     init_screen(scrs);
     get_screen_dimension(scrs);
 
+    // Avoids overriding the standard screen window at the 0th index.
     for (size_t i = 1; i < screen_count; ++i)
         scrs[i].window = init_window();
 
@@ -50,8 +51,9 @@ Screen *setup(void)
 /**
  * @brief Cleans the TUI environment before closure.
  *
- * Frees the dynamically allocated memory for the TUI windows, their associated
- * Dimension structs and the screens array comprising the Screen structs.
+ * @details Frees the dynamically allocated memory for the TUI windows, their
+ *          associated Dimension structs and the screens array comprising the
+ *          Screen structs.
  */
 void clean(Screen *scrs)
 {
