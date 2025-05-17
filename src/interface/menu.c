c27ada0bd61d0743ec8a98711124c106244591ab
rahul4732saini <rahul4732saini@gmail.com>
2025-05-17 15:24:11 +0530

    Refactor show_main_menu function in menu.c
    Removed redundant code, and refactored comments and
    documentation in the function.

--- src/interface/menu.c
+++ src/interface/menu.c
@@ -114,17 +114,15 @@ void init_pause_menu(WinContext *wctx, Dimension *scr_dim)
 /**
  * @brief Displays the main menu window on the TUI screen.
  *
- * @details Displays the main menu window with the menu items also highlighting
- *          the selected item in the specified color pair marking the selection.
+ * @details Displays the main menu window with the menu items
+ * also highlighting the selected item marking the selection.
  *
- * @param scr Pointer to the Screen struct for the main menu.
+ * @param wctx Pointer to the WinContext struct comprising the window data.
  * @param select Index of the selected menu item.
- * @param select_color Integer signifying the color pair for marking selection.
  */
-void show_main_menu(Screen *scr, size_t select, int select_color)
+void show_main_menu(WinContext *wctx, size_t select)
 {
-    WINDOW *win = scr->window;
-    box(win, 0, 0);
+    WINDOW *win = wctx->window;
 
     size_t width = main_menu_width - 2;
     size_t length, left_cutoff;
@@ -140,35 +138,19 @@ void show_main_menu(Screen *scr, size_t select, int select_color)
         // color based on the index of the menu item.
 
         if (i == select)
-            wattron(win, COLOR_PAIR(select_color));
+            wattron(win, COLOR_PAIR(COLOR_SELECT));
 
         wmove(win, i + 1, 1);
-
-        // Explicitly prints the left and right blank spaces to display
-        // the foreground color in case the current item is selected.
 
         wprintw(win, "%*s", left_cutoff, "");
         wprintw(win, "%s", main_menu_items[i]);
         wprintw(win, "%*s", width - length - left_cutoff, "");
 
         if (i == select)
-            wattroff(win, COLOR_PAIR(select_color));
+            wattroff(win, COLOR_PAIR(COLOR_SELECT));
     }
 
     wrefresh(win);
-}
-
-void init_pause_menu(Screen *scr, Dimension *scr_dim)
-{
-    Dimension *dim = scr->dimension;
-
-    dim->height = pause_menu_items_size + 2;
-    dim->width = pause_menu_width;
-
-    dim->start_y = (scr_dim->height - dim->height) / 2;
-    dim->start_x = (scr_dim->width - dim->width) / 2;
-
-    scr->window = init_window(dim);
 }
 
 /**
