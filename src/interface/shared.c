b7e84fd0cf24b647e77a171a789a605aa83462e7
rahul4732saini <rahul4732saini@gmail.com>
2025-05-17 15:31:40 +0530

    Refactor init_window function in shared.c
    Refactored the function to accept a pointer to the Dimension struct of
    the window to set the window dimensions at the initialization itself.
    Also updated the documentation to include additional details associated
    with the function.

--- src/interface/shared.c
+++ src/interface/shared.c
@@ -12,11 +12,16 @@
 
 /**
  * @brief Initializes and configures a new ncurses TUI window.
+ * @param dim Pointer to the Dimension struct comprising the window dimensions.
  * @return Pointer to the newly created window.
  */
-WINDOW *init_window(void)
+WINDOW *init_window(Dimension *dim)
 {
-    WINDOW *win = newwin(0, 0, 0, 0);
+    WINDOW *win = newwin(
+        dim->height,
+        dim->width,
+        dim->start_y,
+        dim->start_x);
 
     refresh();
     keypad(win, TRUE);
