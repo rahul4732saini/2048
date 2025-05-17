056d9820386d1f3642c6d51a14cdfd6efb957c15
rahul4732saini <rahul4732saini@gmail.com>
2025-05-17 15:19:00 +0530

    Define init_pause_menu function in menu.c
    Defined the function for initialzing the pause menu
    and displaying its static layout on the TUI screen.

--- src/interface/menu.c
+++ src/interface/menu.c
@@ -87,6 +87,31 @@ void init_main_menu(WinContext *wctx, Dimension *scr_dim)
 }
 
 /**
+ * @brief Initializes the pause menu and displays its
+ * static layout on the TUI screen.
+ *
+ * @param wctx Pointer to the WinContext struct comprising the window data.
+ * @param scr_dim Pointer to the Dimension struct comprising the
+ * screen dimensions.
+ */
+void init_pause_menu(WinContext *wctx, Dimension *scr_dim)
+{
+    Dimension *dim = wctx->dimension;
+
+    dim->height = pause_menu_items_size + 2;
+    dim->width = pause_menu_width;
+
+    dim->start_y = (scr_dim->height - dim->height) / 2;
+    dim->start_x = (scr_dim->width - dim->width) / 2;
+
+    wctx->window = init_window(dim);
+
+    box(wctx->window, 0, 0);
+    show_menu_heading(scr_dim, pause_menu_title, dim->start_y);
+    show_header_footer(scr_dim);
+}
+
+/**
  * @brief Displays the main menu window on the TUI screen.
  *
  * @details Displays the main menu window with the menu items also highlighting
