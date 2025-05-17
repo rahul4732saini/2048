0535a089cebfebc221c3b29f23c4b9efa610bac6
rahul4732saini <rahul4732saini@gmail.com>
2025-05-17 20:42:46 +0530

    Remove redundant function signatures in core.h

--- src/include/interface/core.h
+++ src/include/interface/core.h
@@ -5,14 +5,6 @@
 #include "shared.h"
 
 void init_screen(Screen *scr);
-void get_screen_dimension(Screen *scr);
-void place_game_win(Screen *scr, Screen *parent);
-
-void show_header_footer(Screen *scr);
-void show_game_win(Screen *scr);
-void show_window_title(Screen *scr, const char *title);
 void show_dialog(Screen *scr, const char *mesg[], size_t mesg_len, int select_color);
-
-void refresh_game_win(Screen *game_scr, Screen *core_scr, const char *title);
 
 #endif
