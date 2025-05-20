023e3010395deafe722bfbaa35f15b189c0be6b5
rahul4732saini <rahul4732saini@gmail.com>
2025-05-20 16:00:04 +0530

    Refactor show_dialog function in core.c
    Removed redundant code blocks within the function definition, and
    refactored comments and documentation.

--- src/interface/core.c
+++ src/interface/core.c
@@ -41,47 +41,40 @@ void init_screen()
  * message at the center of the game window, with an OK button displayed in the
  * specified selection color pair for accepting the text.
  *
- * @param scr Pointer to the Screen struct comprising the game window.
  * @param mesg Pointer to the array comprising the text as inidividual lines.
- * @param mesg_length Length of the mesg array.
- * @param select_color Integer signifying the color pair for marking selection.
+ * @param mesg_len Length of the message array signifying the number of lines.
+ * @param scr_dim Pointer to the Dimension struct comprising the
+ * screen dimensions.
  */
-void show_dialog(Screen *scr, const char *mesg[], size_t mesg_len)
+void show_dialog(const char *mesg[], size_t mesg_len, Dimension *scr_dim)
 {
-    Dimension *dim = scr->dimension;
-    WINDOW *win = scr->window;
-
-    // Calculates the current Y coordinates for placing the message.
-    // A margin of 2 rows is reduced during the  calculation of the
-    // centre position for the OK button.
-    size_t cur_x, cur_y = (dim->height - mesg_len - 2) / 2;
+    // Calculates the Y coordinates for placing the message. A margin of
+    // 2 rows is reduced for the placement of the OK button in the center.
+    size_t pos_x, pos_y = (scr_dim->height - mesg_len - 2) / 2;
 
     for (size_t i = 0; i < mesg_len; ++i)
     {
-        // Calculates the left cutoff to place the text in the middle
-        // of the game window.
-        cur_x = (dim->width - strlen(mesg[i])) / 2;
+        // Calculates the left cutoff to place the text in the center
+        // of the TUI screen.
+        pos_x = (scr_dim->width - strlen(mesg[i])) / 2;
 
-        wmove(win, cur_y++, cur_x);
-        wprintw(win, "%s", mesg[i]);
+        move(++pos_y, pos_x);
+        printw("%s", mesg[i]);
     }
 
-    cur_x = (dim->width - dialog_bt_width) / 2;
+    pos_x = (scr_dim->width - dialog_bt_width) / 2;
 
     // Stores the internal left cutoff within the OK button
-    // to place the text in the middle of the button.
+    // to place the text in the center of the button.
     size_t inner_left_cutoff = (dialog_bt_width - strlen(dialog_bt_txt)) / 2;
 
-    wmove(win, ++cur_y, cur_x);
-    wattron(win, COLOR_PAIR(COLOR_SELECT));
+    move(++pos_y, pos_x);
+    attron(COLOR_PAIR(COLOR_SELECT));
 
-    // Explicitly prints the left and right blank spaces
-    // to display the foreground color within the button.
+    printw("%*s", inner_left_cutoff, "");
+    printw("%s", dialog_bt_txt);
+    printw("%*s", dialog_bt_width - inner_left_cutoff - strlen(dialog_bt_txt), "");
 
-    wprintw(win, "%*s", inner_left_cutoff, "");
-    wprintw(win, "%s", dialog_bt_txt);
-    wprintw(win, "%*s", dialog_bt_width - inner_left_cutoff - strlen(dialog_bt_txt), "");
-
-    wattroff(win, COLOR_PAIR(COLOR_SELECT));
-    wrefresh(win);
+    attroff(COLOR_PAIR(COLOR_SELECT));
+    refresh();
 }
