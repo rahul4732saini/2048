dd669040ef6299c97e089d97f155a179ffc8bed9
rahul4732saini <rahul4732saini@gmail.com>
2025-05-21 11:55:27 +0530

    Rename constants for clarity in consts.c

--- src/consts.c
+++ src/consts.c
@@ -28,15 +28,15 @@ const size_t footers_len = 2;
 const char *main_menu_title = "Main Menu";
 const char *pause_menu_title = "Pause Menu";
 
-const char *main_menu_items[] = {"Play", "Quit"};
-const char *pause_menu_items[] = {
+const char *main_menu_options[] = {"Play", "Quit"};
+const char *pause_menu_options[] = {
     "Resume",
     "Quit to Main Menu",
     "Quit Game",
 };
 
-const size_t main_menu_items_size = 2;
-const size_t pause_menu_items_size = 3;
+const size_t main_menu_option_cnt = 2;
+const size_t pause_menu_option_cnt = 3;
 
 // Widths of the main menu and pause menu window,
 // and the width of the dialog box window button.
