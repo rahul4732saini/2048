e251e9123d4bb5530655ba4e5f02a4c503eb869e
rahul4732saini <rahul4732saini@gmail.com>
2025-05-21 12:55:25 +0530

    Reorder constant declarations in consts.h

--- src/include/consts.h
+++ src/include/consts.h
@@ -25,9 +25,9 @@ extern const size_t cell_width;
 extern const size_t cell_height;
 
 extern const char *headers[];
-extern const char *footers[];
-
 extern const size_t headers_len;
+
+extern const char *footers[];
 extern const size_t footers_len;
 
 extern const char *main_menu_title;
@@ -35,19 +35,19 @@ extern const char *pause_menu_title;
 
 extern const char *main_menu_options[];
 extern const size_t main_menu_option_cnt;
+extern const size_t main_menu_width;
+
 extern const char *pause_menu_options[];
 extern const size_t pause_menu_option_cnt;
-
-extern const size_t main_menu_width;
 extern const size_t pause_menu_width;
 
-extern const size_t dialog_bt_width;
 extern const char *dialog_bt_txt;
+extern const size_t dialog_bt_width;
 
 extern const char *win_dialog_txt[];
-extern const char *lost_dialog_txt[];
-
 extern const size_t win_dialog_txt_len;
+
+extern const char *lost_dialog_txt[];
 extern const size_t lost_dialog_txt_len;
 
 extern const size_t main_menu_handlers[];
