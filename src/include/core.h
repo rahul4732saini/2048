15c18c305359b4124ab60e0e670d84d14b3da548
rahul4732saini <rahul4732saini@gmail.com>
2025-05-21 11:02:33 +0530

    Declare Game struct in core.h
    Declared Game struct allowing game data to be accessed by functions
    outside the file scope.

--- src/include/core.h
+++ src/include/core.h
@@ -3,6 +3,8 @@
 
 #include "shared.h"
 
+extern Game game;
+
 size_t handle_main_menu(Dimension *scr_dim);
 size_t handle_pause_menu(Dimension *scr_dim);
 size_t handle_game_board(Dimension *scr_dim);
