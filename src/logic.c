94a26273eb3eee43a7825f748d3e6571ce57e815
rahul4732saini <rahul4732saini@gmail.com>
2025-06-03 17:59:47 +0530

    Refactor comemnts and function docs in logic.c

--- src/logic.c
+++ src/logic.c
@@ -97,12 +97,15 @@ size_t add_horizontal(Game *game, bool to_right)
 }
 
 /**
- * @brief Vertically merges tiles based on the specified direction.
+ * @brief Vertically adds tiles based on the specified direction.
  *
- * @param game Pointer to the game structure comprising the game board.
- * @param up Signifies the direction of the addition operation. A boolean
- *           true signifies a top to bottom operation whereas the other
- *           signifies a bottom to top operation.
+ * @details Adds and merges adjacent equal tiles in all columns
+ * across the game board in the specified direction (top/down),
+ * and updates the game metadata.
+ *
+ * @param game Pointer to the Game struct comprising the game data.
+ * @param up Boolean value to indicate whether to perform a top to
+ * bottom operation. If false, the operation is performed bottom to top.
  *
  * @return Total number of addition operations performed.
  */
@@ -111,9 +114,8 @@ size_t add_vertical(Game *game, bool up)
     size_t start, end, last, operations = 0;
     int8_t dir = up ? 1 : -1;
 
-    // The following conditional statements define the starting
-    // and ending indices for the addition operation based on the
-    // specified direction of operation.
+    // The following conditional statements define the starting and
+    // ending index for the operation based on the specified direction.
 
     if (up)
         start = 0, end = BOARD_SIZE;
@@ -138,8 +140,8 @@ size_t add_vertical(Game *game, bool up)
                 continue;
             }
 
-            // Updates the maximum tile value, game score and the operations
-            // counter. Also resets the last variable to signify incompatibility.
+            // Updates the game metadata and operations counter, and
+            // resets the "last" variable to signify unavailability.
 
             game->board[last][i] *= 2;
             game->board[j][i] = 0;
