b09f18671b36c100c79aae470785d2b8a3037eb1
rahul4732saini <rahul4732saini@gmail.com>
2025-06-05 10:52:27 +0530

    Update init_screen function in core.c
    Updated the `ESCDELAY` variable exported by ncurses and reduced
    its value to 100ms to avoid performance issues during gameplay.

--- src/interface/core.c
+++ src/interface/core.c
@@ -30,6 +30,9 @@ void init_screen()
     noecho();
     start_color();
 
+    // Reduced ESC key delay to 100ms to avoid performance issues.
+    ESCDELAY = 100;
+
     curs_set(0);
     keypad(stdscr, TRUE);
 }
