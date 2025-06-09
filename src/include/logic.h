0391f8934930d1e7e20a556a53fce47bd76d58ba
rahul4732saini <rahul4732saini@gmail.com>
2025-06-09 09:41:54 +0530

    Update tile manipulator function signatures in logic.h
    Renamed the boolean parameter for accepting operating direction
    within the functions to indicate its actual meaning.

--- src/include/logic.h
+++ src/include/logic.h
@@ -11,10 +11,10 @@ void setup_game(Game *game);
 bool is_game_over(Game *game, bool cell_empty);
 bool place_random(Game *game);
 
-bool add_horizontal(Game *game, bool to_right);
-bool add_vertical(Game *game, bool to_bottom);
+bool add_horizontal(Game *game, bool to_left);
+bool add_vertical(Game *game, bool to_top);
 
-bool move_horizontal(Game *game, bool to_right);
-bool move_vertical(Game *game, bool to_bottom);
+bool move_horizontal(Game *game, bool to_left);
+bool move_vertical(Game *game, bool to_top);
 
 #endif
