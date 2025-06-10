0d6c69f7808048e98badda119e3bc41937d43420
rahul4732saini <rahul4732saini@gmail.com>
2025-06-10 11:03:58 +0530

    Update function signatures in menu.h
    Updated the data-type of `select` variable in menu display handler
    functions to `index_t` to semantically represent that it accepts the
    index of the menu option.

--- src/include/interface/menu.h
+++ src/include/interface/menu.h
@@ -5,9 +5,9 @@
 #include "shared.h"
 
 void init_main_menu(WinContext *wctx, Dimension *scr_dim);
-void show_main_menu(WinContext *wctx, size_t select);
+void show_main_menu(WinContext *wctx, index_t select);
 
 void init_pause_menu(WinContext *wctx, Dimension *scr_dim);
-void show_pause_menu(WinContext *wctx, size_t select);
+void show_pause_menu(WinContext *wctx, index_t select);
 
 #endif
